09db2cf446f05d1f3c3b847314edda1238f0253c
Parth Sharma <53979691+ParthhSharma@users.noreply.github.com>
2021-07-04 00:56:53 +0530

    Update script.js

--- Day 10/script.js
+++ Day 10/script.js
@@ -1,6 +1,6 @@
 let images = ["cards/1.png", "cards/2.png", "./cards/3.png", "./cards/4.png", "./cards/5.png", "./cards/6.png", "./cards/7.png", "./cards/8.png", "./cards/9.png", "./cards/10.png"];
 images = [...images,...images];
-let moves=30;
+let moves=40;
 let matches=0;
 let gameActive=true;
 let backs = document.querySelectorAll('.flip-card-back');
@@ -11,25 +11,30 @@ backs.forEach((back)=>{
     images.splice(rand, 1);
     j++;
 });
-var clicked=0;
+var clicked;
 var cards = document.querySelectorAll('.flip-card');
-let i=0;
 cards.forEach(function(card){
-    
-   
     card.addEventListener('click',()=>{
         if(!gameActive) return;
         moves--;
-        // document.querySelector("span").innerHTML = moves;
+        document.querySelector("span").innerHTML = moves;
 
     if(clicked){
+        console.log(card);
         card.firstElementChild.classList.add("flipped");
         gameActive = false;
-        setTimeout(()=>{
-            if(card.firstChild.lastChild.innerHTML !== clicked.firstChild.lastChild.innerHTML){
-                card.firstChild.classList.remove("flipped");
-                clicked.firstChild.classList.remove("flipped");
-            }else{
+        setTimeout(function(){
+            console.log(clicked.id);
+            console.log(card.id);
+            if(card.id===clicked.id){
+
+            }
+            else if((card.firstElementChild.lastElementChild.innerHTML
+                !==clicked.firstElementChild.lastElementChild.innerHTML)){
+                card.firstElementChild.classList.remove("flipped");
+                clicked.firstElementChild.classList.remove("flipped");
+            }
+            else{
                 matches++;
                 card.style.visibility="hidden";
                 clicked.style.visibility="hidden";
@@ -39,8 +44,27 @@ cards.forEach(function(card){
         }, 500);
     }
     else{
-        console.log(card);
+        clicked = card;
         card.firstElementChild.classList.add('flipped');
     }
 });
+});
+setInterval(() => {
+    if(moves === 0){
+        document.querySelector(".popup").style.display="flex";
+        document.querySelector(".container").style.display="none";
+        document.querySelector("#moves").style.display="none";
+        let h2=document.createElement("h2");
+        document.querySelector(".popup > div > h2").innerHTML="You Lose !!";
+    }
+    if(matches === 10){
+        document.querySelector(".popup").style.display="flex";
+        document.querySelector(".container").style.display="none";
+        document.querySelector("#moves").style.display="none";
+        document.querySelector(".popup > div > h2").innerHTML="You Won !!"
+    }
+}, 100);
+
+document.getElementById("reload").addEventListener("click", (e)=>{
+    window.location.reload();
 });
