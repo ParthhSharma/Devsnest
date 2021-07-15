0c69067ed6dfb27e172c350cb89075d787c77cf6
Parth Sharma <53979691+ParthhSharma@users.noreply.github.com>
2021-07-16 01:40:17 +0530

    Add files via upload

---
+++ Day 14/script.js
@@ -0,0 +1,16 @@
+function onFormSubmit(event) {
+  alert("form Submitted !!");
+  console.log('handle',event);
+}
+function pageloaded() {
+  console.log("Page loaded !!");
+}
+function resizeWindow() {
+  console.log("Window resizing...");
+}
+function offline() {
+  console.log("uh oh!! You're Offline");
+}
+function onvideoplay() {
+  console.log("Video ready to play");
+}
