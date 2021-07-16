95ae806099eb9fe9bd1cb6e263e847d7a5951276
Parth Sharma <53979691+ParthhSharma@users.noreply.github.com>
2021-07-16 19:58:55 +0530

    Add files via upload

---
+++ Day 15/README.md
@@ -0,0 +1,2 @@
+# Output of THA Day15
+![Output of THA Day15](output.png)
---
+++ Day 15/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+function MyApp() {
+  return <div>
+  <h1>Hello Peter!!</h1>
+  <p>Welcome to the Multiverse...</p>
+</div>
+}
+ReactDOM.render(
+  <MyApp/>,document.getElementById('root')
+);
