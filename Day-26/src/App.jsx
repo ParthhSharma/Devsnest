8ea8f143a1d6e064c164b562bcdb01775673e63f
Parth Sharma <53979691+ParthhSharma@users.noreply.github.com>
2021-09-20 11:32:35 +0530

    Add files via upload

---
+++ Day-26/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css';
+import AddTodo from './components/AddTodo';
+import ListTodo from './components/ListTodo';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>TODO LIST</h1>
+      <AddTodo />
+      <ListTodo />
+    </div>
+  );
+}
+
+export default App;
