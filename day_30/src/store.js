10a22551d34e4d87245aade681a8bf174d37660d
Parth Sharma <53979691+ParthhSharma@users.noreply.github.com>
2021-10-04 01:01:46 +0530

    Delete day_30 directory

--- day_30/src/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import allProductsReducer from "./redux/slices/productSlice";
-const store = configureStore({
-  reducer: {
-    allProducts: allProductsReducer,
-  },
-});
-
-export default store;
